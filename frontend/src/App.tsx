9815fbe016f7e6016097ae129441fd9c261c9d8c
Sebastian B <sebasbetancourt14@gmail.com>
2026-06-08 11:38:57 -0500

    feat: implement global toast notification system and integrate into witness registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,19 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import theme from './theme/theme';
 import AppRoutes from './AppRoutes';
+import { ToastProvider } from './context/ToastContext';
+import ToastContainer from './components/ToastContainer';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <ToastProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+        <ToastContainer />
+      </ToastProvider>
     </ThemeProvider>
   );
 }
